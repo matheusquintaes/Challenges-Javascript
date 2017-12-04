5f801960e6d38d7f185aa412d44e3775a8fc339f
matheusquintaes <matheus.quintaes@gmail.com>
2017-12-04 13:32:48 -0200

    challenge 28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -25,3 +25,100 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  (function(doc, win){
+    'use strict'
+
+    var $inputLogradouro = doc.querySelector('[data-js="inputLogradouro"]');
+    var $inputBairro = doc.querySelector('[data-js="inputBairro"]');
+    var $inputEstado = doc.querySelector('[data-js="inputEstado"]');
+    var $inputCidade = doc.querySelector('[data-js="inputCidade"]');
+
+    var $inputCep = doc.querySelector('[data-js="inputCep"]');
+    var $buttonSubmit = doc.querySelector('[data-js="buttonSubmit"]');
+    var ajax = new XMLHttpRequest();
+
+    $buttonSubmit.addEventListener('click', function(e){
+      e.preventDefault();
+      if(!validaCep())
+        return getMessage('minLength');
+      ajaxCorreios();
+    });
+
+    function ajaxCorreios(){
+      getMessage('load');
+      var url = getUrl();
+      ajax.open('GET', url);
+      ajax.send();
+      ajax.addEventListener('readystatechange', handleStateChange);
+    }
+
+    function validaCep(){
+      return justNumbers($inputCep.value).length === 8;
+    }
+
+    function getUrl(){
+      return 'https://viacep.com.br/ws/' + justNumbers($inputCep.value) + /json/;
+    }
+
+    function handleStateChange(){
+      if(isRequestOk())
+        verifyResponse();
+    }
+
+    function verifyResponse() {
+      var data = parseData();
+      fillValues(data);
+      getMessage('ok');
+    }
+
+    function parseData() {
+      var result;
+      try {
+        result = JSON.parse(ajax.responseText);
+      }
+      catch(e){
+        result = null;
+      }
+      return result;
+    }
+
+    function fillValues( data ) {
+      $inputLogradouro.value = data.cep ? data.cep: '';
+      $inputBairro.value = data.bairro ? data.bairro: '';
+      $inputEstado.value = data.uf ? data.uf : '';
+      $inputCidade.value = data.localidade ? data.localidade: '';
+
+      if(data.erro) {
+        clearData();
+        return getMessage('notFound');
+      }
+    }
+
+    function clearData(){
+      $inputLogradouro.value = '';
+      $inputBairro.value = '';
+      $inputEstado.value = '';
+      $inputCidade.value = '';
+    }
+
+    function isRequestOk() {
+      return ajax.readyState === 4 && ajax.status === 200;
+    }
+
+    function justNumbers( string ) {
+      return string.replace(/\D+/g,'');
+    }
+
+    function getMessage( type ){
+      var $spanStatus = doc.querySelector('[data-js="spanStatus"]');
+      var messages = {
+        loading:'Buscando informações para o CEP '+ $inputCep.value + '...',
+        ok:'Endereço referente ao CEP: ' + $inputCep.value,
+        minLength:'O Cep tem que ter 8 dígitos',
+        notFound: 'Não encontramos o endereço para o CEP '+ $inputCep.value + '.'
+      };
+      $spanStatus.textContent = messages[type];
+    }
+
+  })(document, window);
